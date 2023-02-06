53e25686728752f6cd1b69434be3f341295fd64e
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-02-06 17:31:02 +0530

    doubly linked list complete

--- linked_list/doubly_linkedlist.cpp
+++ linked_list/doubly_linkedlist.cpp
@@ -12,29 +12,35 @@ Node (int data){
     this->next=NULL;
 }
 };
-void insertAtHead(Node* &tail,Node*  &head,int data){
 
-    if(head==NULL){
+
+
+void insertAtHead(Node* &tail,Node*  &head,int data){
+     if(head==NULL){
         Node*temp=new Node(data);
         head=temp;
         tail=temp;
     }
-    Node* temp=new Node(data);
+    else{Node* temp=new Node(data);
     temp->next=head;
     head->prev=temp;
-    head=temp;
+    head=temp;}
 }
+
+
+
+
 void insertAtTail(Node* &tail,Node* &head,int data){
      if(tail==NULL){
         Node*temp=new Node(data);
         head=temp;
         tail=temp;
     }
-Node* temp=new Node(data);
+else{Node* temp=new Node(data);
 tail->next=temp;
 temp->prev=tail;
 tail=temp;
-
+}
 }
 void insertAtMiddle(Node* &tail,Node* &head,int data,int pos){
 if (pos==1){
@@ -65,6 +71,39 @@ else
 
 }
 }
+
+void deleteNode(Node* &tail,Node* &head,int position){
+    if (position==1)
+    {
+       Node* temp=head;
+       temp->next->prev=NULL;
+       head=temp->next;
+       temp->next=NULL;
+       delete temp;
+    }
+    else{
+        Node* curr=head;
+        Node* prev=NULL;
+int count=1;
+while (count<position)
+{
+ prev=curr;
+ curr=curr->next;
+ count++;
+    /* code */
+}
+curr->prev=NULL;
+prev->next=curr->next;
+curr->next=NULL;
+delete curr;
+if (prev->next==NULL)
+{
+    tail=prev;
+    /* code */
+}
+
+    }
+}
 void print(Node *&head){
     Node *temp=head;
     while (temp!=NULL){
@@ -73,18 +112,25 @@ void print(Node *&head){
     }
 }
 int main(){
-Node* node1=new Node(10);
-Node* head=node1;
-Node* tail=node1;
+// Node* node1=new Node(10);
+// Node* head=node1;
+// Node* tail=node1;
+// print(head);
+// insertAtHead(head,20);
+// insertAtHead(head,30);
+// insertAtHead(head,40);
+// print(head);
+// insertAtTail(tail,head,20);
+// insertAtTail(tail,head,30);
+// insertAtTail(tail,head,40);
+// print(head);
+Node* head=NULL;
+Node* tail= NULL;
+insertAtHead(tail,head,5);
+insertAtHead(tail,head,4);
+insertAtHead(tail,head,3);
+insertAtHead(tail,head,2);
+deleteNode(tail,head,4);
 print(head);
-insertAtHead(head,20);
-insertAtHead(head,30);
-insertAtHead(head,40);
-print(head);
-insertAtTail(tail,head,20);
-insertAtTail(tail,head,30);
-insertAtTail(tail,head,40);
-print(head);
-insertAtMiddle(tail,head,25,3);
-
+cout<<"tail is"<< tail->data;
 return 0;}
