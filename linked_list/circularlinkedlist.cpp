da7d097b0092202527b871084e3a597eada06484
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-02-07 13:05:19 +0530

    complete circular list

---
+++ linked_list/circularlinkedlist.cpp
@@ -0,0 +1,120 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+Node* next;
+int data;
+Node(int data){
+    this->data=data;
+    this->next=NULL;
+}};
+
+void insert(Node* &tail,int element,int data){
+if (tail==NULL)
+{
+    Node* newNode=new Node(data);
+    tail=newNode;
+    newNode->next=newNode;
+    /* code */
+}
+else{
+    Node* curr=tail;
+while (curr->data!=element)
+{
+    curr=curr->next;
+    /* code */
+}
+Node*temp=new Node(data);
+temp->next=curr->next;
+curr->next=temp;
+tail=temp;
+}
+
+
+}
+void print(Node* tail){
+    Node *temp=tail;
+      if(tail == NULL) {
+        cout << "List is Empty "<< endl;
+        return ;
+    }
+
+do
+{
+    cout<<tail->data<<endl;
+    tail=tail->next;
+} while (tail!=temp);
+
+
+}
+
+void deleteNode(Node* &tail,int element){
+if (tail==NULL)
+{
+    cout<<"No element found"<<endl;
+    /* code */
+    return;
+}
+else{
+Node* prev=tail;
+Node* curr=prev->next;
+    while (curr->data!=element)
+    {
+        prev=curr;
+        curr=curr->next;
+    }
+//1 node
+if (prev==curr)
+{
+    /* code */
+
+    tail=NULL;
+    return;
+}
+
+
+prev->next=curr->next;
+if (tail==curr)
+{
+    tail=prev;
+}
+
+curr->next=NULL;
+delete curr;
+}
+
+
+
+}
+int main(){
+Node* tail=NULL;
+insert(tail,5,6);
+cout<<"tail is"<<tail->data<<endl;
+// print(tail);
+insert(tail,6,16);
+cout<<"tail is"<<tail->data<<endl;
+// print(tail);
+insert(tail,16,26);
+cout<<"tail is"<<tail->data<<endl;
+// print(tail);
+insert(tail,26,36);
+cout<<"tail is"<<tail->data<<endl;
+// print(tail);
+insert(tail,36,46);
+cout<<"tail is"<<tail->data<<endl;
+// print(tail);
+insert(tail,46,56);
+cout<<"tail is"<<tail->data<<endl;
+// print(tail);
+insert(tail,56,66);
+cout<<"tail is"<<tail->data<<endl;
+deleteNode(tail,6);
+deleteNode(tail,16);
+deleteNode(tail,26);
+deleteNode(tail,36);
+deleteNode(tail,46);
+
+print(tail);
+return 0;}
+
