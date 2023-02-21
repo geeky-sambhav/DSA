64d66d60b94791a521c245f0a11c34382a9ccb2a
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-02-21 22:11:25 +0530

    Create removeLoop.cpp

---
+++ linked_list/removeLoop.cpp
@@ -0,0 +1,55 @@
+class Solution
+{
+    public:
+    Node* startOfLoop(Node* head){
+        if(head==NULL){
+            return NULL;
+        }
+        Node* intersect=intersection(head);
+        Node* slow=head;
+        if(intersect==NULL){
+            return NULL;
+        }
+        while(intersect!=slow){
+                intersect=intersect->next;
+                slow=slow->next;
+            }
+            return slow;
+        
+    }
+    Node* intersection(Node* head){
+         if(head==NULL){
+            return NULL;
+        }
+         Node* fast=head;
+      Node* slow=head;
+    while(fast!=NULL && slow!=NULL){
+        fast=fast->next;
+        if(fast!=NULL){
+            fast=fast->next;
+        }
+        slow=slow->next;
+        if(fast==slow){
+            return slow;
+          }
+          }
+       return NULL; 
+    }
+    
+    //Function to remove a loop in the linked list.
+    void removeLoop(Node* head)
+    {
+        if(head==NULL ){
+            return ;
+        }
+        Node* start=startOfLoop(head);
+          if(start==NULL){
+        return ;
+    }
+        Node* temp=start;
+        while(temp->next!=start){
+        temp=temp->next;
+        }
+        temp->next=NULL;
+         }
+};
