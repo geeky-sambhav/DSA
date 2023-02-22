95c244788b465be59a29d3da514fd31ff68d29f8
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-02-22 17:23:35 +0530

    Start of Loop

---
+++ linked_list/startOfLoop.cpp
@@ -0,0 +1,34 @@
+Node* startOfLoop(Node* head){
+        if(head==NULL){
+            return NULL;
+        }
+        Node* intersect=intersection(head);
+        Node* slow=head;
+        if(intersect==NULL){
+            return NULL;
+        }
+        while(intersect!=slow){
+                intersect=intersect->next;
+                slow=slow->next;
+            }
+            return slow;
+        
+    }
+    Node* intersection(Node* head){
+         if(head==NULL){
+            return NULL;
+        }
+         Node* fast=head;
+      Node* slow=head;
+    while(fast!=NULL && slow!=NULL){
+        fast=fast->next;
+        if(fast!=NULL){
+            fast=fast->next;
+        }
+        slow=slow->next;
+        if(fast==slow){
+            return slow;
+          }
+          }
+       return NULL; 
+    }
