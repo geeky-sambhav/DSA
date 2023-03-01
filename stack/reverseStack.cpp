7f052aa762461cc95d0fcac4fc7b47a0fb71cb05
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-03-01 19:55:06 +0530

    Reverse Stack

---
+++ stack/reverseStack.cpp
@@ -0,0 +1,24 @@
+
+void insertAtbottom(stack<int> &stack,int num){
+
+if(stack.empty()){
+    stack.push(num);
+    return;
+}
+else{
+    int number=stack.top();
+    stack.pop();
+    insertAtbottom(stack,  num);
+    stack.push(number);
+}
+}
+void reverseStack(stack<int> &stack) {
+ if(stack.empty()){
+     return;
+ }
+int num=stack.top();
+stack.pop();
+reverseStack(stack);
+insertAtbottom(stack,num);
+
+}
