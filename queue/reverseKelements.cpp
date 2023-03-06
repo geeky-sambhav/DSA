dab7ac7127ccfcd9f68a38468eeeb4dc09494fae
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-03-06 17:29:41 +0530

    Reverse k elements

---
+++ queue/reverseKelements.cpp
@@ -0,0 +1,16 @@
+queue<int> modifyQueue(queue<int> q, int k) {
+    stack<int>s1;
+    
+    for(int i=0;i<k;i++){
+        s1.push(q.front());
+        q.pop();
+    }
+    while(!s1.empty()){
+        q.push(s1.top());
+        s1.pop();
+    }
+ for(int i=0;i<q.size()-k;i++){
+     q.push(q.front());
+     q.pop();}
+    return q;
+}
