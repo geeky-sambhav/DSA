80b15c6882a0e7a9f81f53ee2f1449b500cdf075
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-03-13 12:52:10 +0530

    Update creation.cpp

--- trees/creation.cpp
+++ trees/creation.cpp
@@ -27,6 +27,39 @@ root->left=buildtree(root->left);
 root->right=buildtree(root->right);
 return root;
 }
+void buildFromLevelOrder(node* &root) {
+    queue<node*> q;
+
+    cout << "Enter data for root" << endl;
+    int data ;
+    cin >> data;
+    root = new node(data);
+    
+    q.push(root);
+
+    while(!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        cout << "Enter left node for: " << temp->data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if(leftData != -1) {
+            temp -> left = new node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter right node for: " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if(rightData != -1) {
+            temp -> right = new node(rightData);
+            q.push(temp->right);
+        }
+    }
+ }
 
 void levelOrderTraversal(node* root) {
     queue<node*> q;
