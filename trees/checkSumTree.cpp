c81e31ef9070e164a1a708bf3dcce2f2803f9727
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-03-15 11:52:34 +0530

    Create checkSumTree.cpp

---
+++ trees/checkSumTree.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+    int solve(Node* root,int &ans){
+        if(!root){
+            return 0;
+        }
+        if(!root->left && !root->right){
+            return root->data;
+        }
+        if(ans==0){
+            return 0;
+        }
+        int lt=solve(root->left,ans);
+        int rt=solve(root->right,ans);
+        if(lt+rt!=root->data){
+            return ans=0;
+        }   
+        return (lt+rt+root->data) ;
+    }
+    bool isSumTree(Node* root)
+    {
+        int ans=1;
+        int a=solve(root,ans);
+        if(ans!=1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+};
