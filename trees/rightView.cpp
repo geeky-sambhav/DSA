f6f6ebc7344049d0af20f3f88f314a47fbdcf8cf
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-03-16 13:19:46 +0530

    Create rightView.cpp

---
+++ trees/rightView.cpp
@@ -0,0 +1,25 @@
+SAME AS LEFT BUT PEHLE RIGHT SE VIEW KREGE
+
+class Solution
+{
+    public:
+    void solve(Node *root,int lvl,vector<int> &ans){
+    if(root==NULL){
+        return;
+    }
+    //THIS MEANS U HAVE REACHED A NEW LEVEL
+    if(lvl==ans.size()){
+ans.push_back(root->data);
+    }
+    solve(root->right,lvl+1,ans);
+    solve(root->left,lvl+1,ans);
+}
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> rightView(Node *root)
+    {vector<int>ans;
+   
+   solve(root,0,ans);
+   return ans;
+       // Your Code here
+    }
+};
