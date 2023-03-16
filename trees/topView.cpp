75b4dc0c3f3d9f337186965ddb9e8938289bdcd2
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-03-16 12:26:04 +0530

    Create topView.cpp

---
+++ trees/topView.cpp
@@ -0,0 +1,43 @@
+JB BHI UPAR SE NICHE SE DEKHNE VALA QUESTION AA JAYE TO LEVEL WISE DEKHEGE
+
+
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        map<int,int> nodes;
+        vector<int>ans;
+        if(root==NULL){
+            return ans;
+        }
+        queue<pair<Node*,int>>q;
+        q.push(make_pair(root,0));
+        while(!q.empty()){
+            pair<Node*,int>temp=q.front();
+            q.pop();
+            Node* frontNode=temp.first;
+            int hd=temp.second;
+            if(nodes.find(hd)==nodes.end()){
+                nodes[hd]=frontNode->data;
+                
+                
+            }
+            if(frontNode->left){
+                q.push(make_pair(frontNode->left,hd-1));
+            }
+            if(frontNode->right){
+                q.push(make_pair(frontNode->right,hd+1));
+            }
+            
+        }
+        for(auto i : nodes){
+            ans.push_back(i.second);
+        }
+        
+        
+    }
+
+};
