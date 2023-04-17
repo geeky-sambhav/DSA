92a124da32234d30cfeff4b9b159b6740a5f2c88
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-04-17 15:12:43 +0530

    Create findMedianInAStream.cpp

---
+++ heap/findMedianInAStream.cpp
@@ -0,0 +1,54 @@
+class Solution
+{
+    public:
+    //Function to insert heap.
+        priority_queue<int>s;
+        priority_queue<int,vector<int>,greater<int>>g;
+    void insertHeap(int &x)
+    {
+        
+        if(s.size()==0){
+            s.push(x);
+            return;
+        }
+        if(s.size()>g.size()){
+            if(x<s.top()){
+                g.push(s.top());
+                s.pop();
+                s.push(x);
+            }
+            else{
+                g.push(x);
+            }
+            }
+            else{
+                if(x<s.top()){
+                    s.push(x);
+                }
+                else{
+                    g.push(x);
+                    s.push(g.top());
+                    g.pop();
+                }
+                }
+            
+    }
+    
+    
+    //Function to balance heaps.
+    void balanceHeaps()
+    {
+        
+    }
+    
+    //Function to return Median.
+    double getMedian()
+    {
+        if(g.size()==s.size()){
+            return (double)(s.top()+g.top())/2.0;
+        }
+        else{
+            return s.top();
+        }
+    }
+};
