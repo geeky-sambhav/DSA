7ed3d2b3ad93612edaa07cc95b2549fffb0fdfd2
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-04-25 02:49:32 +0530

    Update addTwoLinkedList.cpp

--- linked_list/addTwoLinkedList.cpp
+++ linked_list/addTwoLinkedList.cpp
@@ -1,65 +1,26 @@
-class Solution
-{
-    public:
-    void insertAtTail(Node* &head,Node* &tail,int val){
-        Node* temp=new Node(val);
-        if(head==NULL){
-            head=temp;
-            tail=temp;
-        }
-        else{
-            tail->next=temp;
-            tail=temp;
-        }
-    }
-    struct Node* reverse(struct Node* head){
-        Node* curr=head;
-        Node* prev=NULL;
-        Node* next=NULL;
-        while(curr!=NULL){
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-        }
-        return prev;
-    }
-    struct Node* add(struct Node* first, struct Node* second){
-        Node* anshead=NULL;
-        Node* anstail=NULL;
-        int carry=0;
-        while(first!=NULL || second!=NULL || carry!=0){
-        int val1=0;
-        int val2=0;
-            if(first){ 
-                val1=first->data;
-        }
-      
-        if(second){
-            val2=second->data;
-        }
-        
-        int sum=val1+val2+carry;
-        carry=sum/10;
-        int digit=sum%10;
-        insertAtTail(anshead,anstail,digit);
-        if(first){
-            first=first->next;
-            
-        }
-        if(second){
-            second=second->next;
-        }
-    }
-    return anshead;}
-    //Function to add two numbers represented by linked list.
-    struct Node* addTwoLists(struct Node* first, struct Node* second)
-    {
-        first=reverse(first);
-        second=reverse(second);
-        Node* ans=add(first,second);
-        ans=reverse(ans);
-        return ans;
-        // code here
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+            ListNode* dummy=new ListNode(0);
+            ListNode* temp=dummy;
+           
+            int carry=0; 
+            while(l1!=NULL || l2!=NULL || carry!=0){
+               int sum=0;
+               if(l1){
+                   sum+=l1->val;
+                   l1=l1->next;
+                   }
+              if(l2){
+                   sum+=l2->val;
+                   l2=l2->next;
+                   }
+                sum+=carry;
+                carry=sum/10;
+                ListNode* node=new ListNode(sum%10);
+                temp->next=node;
+                temp=temp->next;
+            }
+            return dummy->next;
     }
 };
