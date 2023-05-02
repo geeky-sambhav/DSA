2e860fe2797e0112ff32cc94a5c30fb8fca25786
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-05-02 16:39:30 +0530

    Update bfs.cpp

--- graphs/bfs.cpp
+++ graphs/bfs.cpp
@@ -3,17 +3,17 @@ class Solution {
     // Function to return Breadth First Traversal of given graph.
     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
        queue<int>q;
-       int arr[V]={0};
+       int vis[V]={0};
        vector<int>bfs;
-       q.push(arr[0]);
-       arr[0]=1;
+       q.push(vis[0])// or put the first element;
+       vis[0]=1;
        while(!q.empty()){
            int node=q.front();
            q.pop();
            bfs.push_back(node);
            for(auto it:adj[node]){
-               if(!arr[it]){
-                   arr[it]=1;
+               if(!vis[it]){
+                   vis[it]=1;
                    q.push(it);
                }
            }
