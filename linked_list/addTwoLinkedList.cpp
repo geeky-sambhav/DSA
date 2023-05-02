57ac059e58fa695332e2d2b050f9a2e3648cba23
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-05-02 12:35:16 +0530

    Update addTwoLinkedList.cpp

--- linked_list/addTwoLinkedList.cpp
+++ linked_list/addTwoLinkedList.cpp
@@ -3,8 +3,7 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
             ListNode* dummy=new ListNode(0);
             ListNode* temp=dummy;
-           
-            int carry=0; 
+                       int carry=0; 
             while(l1!=NULL || l2!=NULL || carry!=0){
                int sum=0;
                if(l1){
