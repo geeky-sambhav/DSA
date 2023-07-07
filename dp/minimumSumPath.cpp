3c7806cf905c9084e13fcc32c5e214c051bfa310
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-07-07 22:59:22 +0530

    Create minimumSumPath.cpp

---
+++ dp/minimumSumPath.cpp
@@ -0,0 +1,34 @@
+
+
+int minSumPath(vector<vector<int>> &grid) {
+  int n=grid.size();
+  int m=grid[0].size();
+  
+
+
+int dp[n][m];
+for (int i=0;i<n;i++){
+    for (int j=0;j<m;j++){
+            if(i==0 && j==0){
+                dp[i][j]=grid[0][0];
+            }else{
+int up=INT_MAX;
+int left=INT_MAX;
+if(i>0){
+up=grid[i][j]+dp[i-1][j];
+}
+if(j>0){
+    left=grid[i][j]+dp[i][j-1];
+    
+}
+dp[i][j]=min(up,left);
+            }
+
+    }
+
+}
+return dp[n-1][m-1];
+ 
+
+
+}
