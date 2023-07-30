a4da556cb5b76dac15df33df6739c66a1bf890c7
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-07-30 14:00:06 +0530

    Create topView.cpp

---
+++ treeStriver/topView.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+vector<int>ans;
+if(root==NULL) return ans;
+map<int,int>mapping;
+queue<pair<Node*,int>>q;
+q.push({root,0});
+while(!q.empty()){
+    auto p=q.front();
+    q.pop();
+   Node* node=p.first;
+    int x=p.second;
+    if(mapping.find(x)==mapping.end()) mapping[x]=node->data;
+    if(node->left) q.push({node->left,x-1});
+    if(node->right) q.push({node->right,x+1});
+}
+for(auto i:mapping){
+    ans.push_back(i.second);
+}
+return ans;
+    }
+
+};
